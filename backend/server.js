16920b17d8eeff0e6fb2d9c395e48952b817ac90
rhit-trudtoj <78326617+rhit-trudtoj@users.noreply.github.com>
2022-07-25 18:11:44 -0400

    yuh

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,20 @@ const express = require("express"),
 const bodyParser = require('body-parser');
 const fs = require("fs");
 
+const basicAuth = require("express-basic-auth");
+var { authenticator, upsertUser, cookieAuth } = require("./authentication");
+const auth = basicAuth({
+    authorizer: authenticator
+});
+const cookieParser = require("cookie-parser");
+app.use(cookieParser("82e4e438a0705fabf61f9854e3b575af"));
+
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}));
+
+
 app.use(cors());
 app.use(bodyParser.json({ extended: true }));
 app.listen(port, () => console.log("Backend server live on " + port));
@@ -49,7 +63,7 @@ app.get("/get/items", getItems)
     //uncomment to see the data being returned 
     //console.log(JSON.parse(data));
 
-    response.json(JSON.parse(data));
+    response.send(JSON.parse(data));
     // Note this won't work, why? response.send();
   } 
 
@@ -66,6 +80,24 @@ app.get("/get/searchitem",searchItems)
 
     //uncomment to see the todolists found in the backend service// 
     //console.log(returnData);
-    response.json(returnData);
+    response.send(returnData);
     //Note this won't work, why? response.send();
   }
+
+  app.get("/authenticate", auth, (req, res) => {
+    console.log(`user logging in: ${req.auth.user}`);
+    res.cookie('user', req.auth.user, { signed: true });
+    res.sendStatus(200);
+});
+
+app.post("/users", (req, res) => {
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+    const [username, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+    const upsertSucceeded = upsertUser(username, password)
+    res.sendStatus(upsertSucceeded ? 200 : 401);
+});
+
+app.get("/logout", (req, res) => {
+    res.clearCookie('user');
+    res.end();
+});
